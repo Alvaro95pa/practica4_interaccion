f8fd7bdabdff9158c3df9fa01d85f6332b534e45
Alvaro <a.pavonal@alumnos.urjc.es>
2019-11-16 19:33:50 +0100

    Apartado 3: Avance progresivo shake your head

--- Assets/WorldControlVR.cs
+++ Assets/WorldControlVR.cs
@@ -1,44 +1,75 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class WorldControlVR : MonoBehaviour {
+public class WorldControlVR : MonoBehaviour
+{
 
-	public CameraControl cameraControl = null;
-	private Vector3 position = new Vector3 (0.0f, 0.0f, 0.0f);
+    public CameraControl cameraControl = null;
+    private Vector3 position = new Vector3(0.0f, 0.0f, 0.0f);
+    private float lastVarX = 0.0f;
+    private float speed = 10.0f;
+    private Vector3 myPosition = new Vector3(0.0f, 0.0f, 0.0f);
+    private float vertical = 0.0f;
+    private float lastVertical;
 
-	void Start () {
-	
-	}
-	
-
-	void Update () {
-
-		//Rotatción del eje y
-		float yRotation = 0.0f;
-		if (cameraControl) {
-			yRotation = cameraControl.yRotation;
-		}
-
-		//Shake your head
-		// variación en el eje x
-		float varX = 0.0f;
-
-		// Depuración en entorno Unity
-		if (cameraControl){
-			varX = cameraControl.zRotation;
-		}
-		// Obtención de los datos del acelerometro para correr en el dispositivo movil
-//		varX = Input.acceleration.x;
-
-		//Codigo práctica
-		//**************************************//
+    void Start()
+    {
+        position += Vector3.down; //Desplazamos el mundo un (-1) por debajo de la cámara
+    }
 
 
+    void Update()
+    {
 
+        //Rotatción del eje y
+        float yRotation = 0.0f;
+        if (cameraControl)
+        {
+            yRotation = cameraControl.yRotation;
+        }
 
+        //Shake your head
+        // variación en el eje x
+        float varX = 0.0f;
 
-		//**************************************//
+        // Depuración en entorno Unity
+        if (cameraControl)
+        {
+            varX = cameraControl.zRotation;
 
-		this.transform.position = position;
-	}
+        }
+        else
+        {
+            // Obtención de los datos del acelerometro para correr en el dispositivo movil
+            varX = Input.acceleration.x;
+        }
+
+        //Codigo práctica
+        //**************************************//
+
+        float t = Time.deltaTime;
+
+        Vector3 forwardVector = Vector3.forward;
+
+        float resta = lastVarX - varX;
+
+        if (Mathf.Abs(resta) > 2)
+        {
+            if(lastVertical < 1.0f) vertical += 0.01f;
+            myPosition += (-forwardVector) * vertical * speed * t;
+        }
+        else
+        {
+            vertical = 0.0f;
+        }
+
+        position = myPosition;
+        position = Quaternion.AngleAxis(yRotation, Vector3.up) * position;
+
+        //**************************************//
+        
+        this.transform.position = position;
+        lastVarX = varX;
+        lastVertical = vertical;
+    }
 }
